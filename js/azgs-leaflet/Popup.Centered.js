9d88fe31515444f21613481851c98d30ec09f2c4
Janel <janel.day@azgs.az.gov>
2012-12-13 16:10:24 -0700

    updated map icon, swapped osm basemap for custom basemap

--- js/azgs-leaflet/Popup.Centered.js
+++ js/azgs-leaflet/Popup.Centered.js
@@ -6,19 +6,17 @@ L.Popup.Centered = L.Popup.extend({
 		options.className += "centered-popup";
 		L.Popup.prototype.initialize.call(this, options, source);
 	},
-
+	
 	_initLayout: function() {
 		L.Popup.prototype._initLayout.call(this);
 		this._container.removeChild(this._tipContainer);
 	},
-
+	
 	_updatePosition: function() {
 		var mapSize = this._map.getSize(),
-		pos = this._map.containerPointToLayerPoint(new L.Point(Math.round(mapSize.x / 2),Math.round(mapSize.y / 2)));
-	
-		this._containerHeight = this._container.clientHeight;
+			pos = this._map.containerPointToLayerPoint(new L.Point(Math.round(mapSize.x / 2),Math.round(mapSize.y * 0.15)));
 		
-		this._containerTop = pos.y - Math.round(this._containerHeight / 2) - this.options.offset.y;
+		this._containerTop = pos.y - this.options.offset.y;
 		this._containerLeft = pos.x - Math.round(this._containerWidth / 2) + this.options.offset.x;
 		
 		this._container.style.top = this._containerTop + 'px';
